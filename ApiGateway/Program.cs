530660355c3652c91715c5f96f576bdccf21da05
nguyen5562 <nknguyen112345@gmail.com>
2025-05-17 00:18:16 +0700

    fix method options

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,6 +3,16 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Đọc cấu hình từ ocelot.json
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
@@ -11,6 +21,7 @@ builder.Services.AddOcelot();
 
 var app = builder.Build();
 
+app.UseCors();
 // Dùng Ocelot Middleware
 await app.UseOcelot();
 
